a0d574d51ffbdbe30e21597c37a9728bd4337f34
Reza  Nehzati <rezanehzati@Rezas-MacBook-Pro.local>
2019-02-27 20:40:34 +0300

    update

--- craco.config.js
+++ craco.config.js
@@ -1,24 +1,24 @@
 // Don't open the browser during development
 process.env.BROWSER = 'none';
 const CracoLessPlugin = require('craco-less');
-// const CracoAntDesignPlugin = require('craco-antd');
+const CracoAntDesignPlugin = require('craco-antd');
 
 module.exports = {
   plugins: [
-    // {
-    //   plugin: CracoAntDesignPlugin,
-    //   options: {
-    //     customizeTheme: {
-    //       '@font-size-base': '22px'
-    //     }
-    //   }
-    // },
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeTheme: {
+          '@font-size-base': '12px'
+        }
+      }
+    },
     {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
           modifyVars: {
-            '@font-size-base': '22px'
+            '@font-size-base': '12px'
           },
           javascriptEnabled: true
         }
